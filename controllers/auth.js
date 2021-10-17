4b35387971b4c6c40bef9cbca1e30319301c846f
Sagar9081 <83939401+Sagar9081@users.noreply.github.com>
2021-10-17 11:53:35 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ exports.forgotpassword = async (req, res, next) => {
     const resetToken = user.getResetPasswordToken();
     await user.save();
 
-    const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+    const resetUrl = `https://login-auth12.netlify.app/passwordreset/${resetToken}`;
     const message = `
      <h1>You Have requested a password reset </h1>
      <p>Please go this link to reset your password</p>
